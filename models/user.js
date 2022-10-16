6a62a6a4ed7e04a5f4341f7bcd04a4392aeec0af
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-10-16 20:57:14 +0300

    fix

--- models/user.js
+++ models/user.js
@@ -33,7 +33,6 @@ const registerSchema = Joi.object({
     email: Joi.string().pattern(emailRegexp).required(),
     password: Joi.string().min(6).required(),
     subscription:Joi.string(),
-    token:Joi.string(),
 })
 
 const loginSchema = Joi.object({
